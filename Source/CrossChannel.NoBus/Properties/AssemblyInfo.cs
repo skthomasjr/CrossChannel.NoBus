4644e424bf9b2ab4eb1eecf362d7eeb38cd706f8
Scott Keith Thomas, Jr <sthomas@idis.com>
2016-06-14 08:03:38 -0600

    Initial commit.

---
+++ Source/CrossChannel.NoBus/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CrossChannel.NoBus")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CrossChannel.NoBus")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("326e8b28-cf4f-4842-b826-e3db888240d1")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
